0a79ab671df5a0019713d26829025c5711043e5a
Sebastian <joansebasfranco@hotmail.com>
2020-12-07 03:12:22 -0500

    Fin sección 10 - Backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,19 +1,29 @@
-const {io} = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje} = require('../controllers/socket');
 
-//Mensajes de Sockets
-io.on('connection', client => {
+// Mensajes de Sockets
+io.on('connection', (client) => {
 
-    console.log('Cliente conectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
 
-    client.on('disconnect', () => { 
-        console.log('Cliente desconectado');
+    if (!valido) { return client.disconnect(); }
+
+    //Cliente autenticado
+    usuarioConectado(uid);
+
+    // Ingresar al usuario a una sala en particular
+    client.join( uid );
+
+    // Escuchar del cliente el mensaje personal
+    client.on('mensaje-personal', async (payload) => {
+        //TODO: grabar mensaje
+        await grabarMensaje( payload );
+        io.to(payload.para).emit('mensaje-personal', payload);
     });
 
-    // client.on('mensaje', (payload) => {
-    //     console.log('Mensaje', payload);
-
-    //     io.emit('mensaje', { admin: 'Nuevo mensaje' });
-
-    // });
+    client.on('disconnect', () => {
+        usuarioDesconectado(uid);
+    });
 
 });
